32263c25934dc656abf3a03dfe2e8c76f8c8dfeb
Dasa Ponnappan <dasa@manizh.com>
2022-02-18 12:54:45 -0500

    Checking in changes for folders

--- index.js
+++ index.js
@@ -14,21 +14,38 @@ async function handleRequest(request) {
   
   console.log("The folders request url is " + request)
   const urlPath = new URL(request.url)
+  const params = new URLSearchParams(urlPath.search)
+  console.log("The(Folders) params Query String is" + params.toString())  
+  console.log("The(Folders) urlPath Object is " + urlPath);
   const value = await FOLDER_REWRITE_MAP.get(urlPath.pathname)
   const redirectValue = await FOLDER_REDIRECT_MAP.get(urlPath.pathname)
-  console.log("The URL to be redirected for the provided URL:"+urlPath.pathname+" is :"+value)  
+  console.log("The Folders URL to be redirected for the provided URL:"+urlPath.pathname+" is :"+value)  
   urlVal=""
   isRewrite = false
   isRedirectPath = false
   if(redirectValue) {
     urlVal = "https://"+urlPath.host+redirectValue
+    if(params.toString() != "") {
+      urlVal = urlVal + "?" + params.toString()
+    }
+    console.log("(Folders Redirect) The Query String to be apppended to urlVal "+params.toString())        
     isRedirectPath = true
   }
   else if(value) {
     urlVal = "https://"+urlPath.host+value
+    if(params.toString() != "") {
+      urlVal = urlVal + "?" + params.toString()
+    }
+    console.log("(Folders Rewrite) The Query String to be apppended to urlVal "+params.toString())            
     isRewrite = true
   }  else {
     urlVal = request.url
+    //Append params to urlVal if params is not empty
+    if(params.toString() != "") {
+      urlVal = urlVal + "?" + params.toString()
+    }
+    console.log("The Folders Query String to be apppended to urlVal "+params.toString())
+    console.log("Since no path matched (Folders) sending to URL Val"+urlVal)    
   }
   if(isRewrite) {
     return fetch(new Request(urlVal, {
